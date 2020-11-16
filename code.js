7a313f9eaae4d8866dcd16c886dd0efe9b7a9fb4
buetowcj <cbuetow@my.wctc.edu>
2020-11-15 20:42:44 -0600

    Update commit

--- code.js
+++ code.js
@@ -1,9 +1,36 @@
 $(document).ready(
-    function (dayOfWeek) {
+    function () {
 // add event listener (clicks, etc.)
-switch (dayOfWeek) {
-    //case "Monday"
-}
+        var selectedDay = $("input[name=day]:checked").val();
+        $(":radio").change(showSchedule);
+
+        function showSchedule() {
+
+        switch(selectedDay)
+        {
+            case 0:
+                day = Monday;
+                break;
+            case 1:
+                day = Tuesday;
+                break;
+            case 2:
+                day = Wednesday;
+                break;
+            case 3:
+                day = Thursday;
+                break;
+            case 4:
+                day = Friday;
+                break;
+            case 5:
+                day = Saturday;
+                break;
+            case 6:
+                day = Sunday;
+                break;
+        }
+    }
 
 // add all other functions here
 
